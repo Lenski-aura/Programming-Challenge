a3fb9fe858a1d3c398cd6a3173d11d62ddf9521b
Lennon <bty25ywu@UEA.ac.uk>
2026-03-10 09:41:22 +0000

    Added algorithm

--- File Program/fileReader.java
+++ File Program/fileReader.java
@@ -24,7 +24,7 @@ public class fileReader {
                     lines.add(line);
                 }
 
-        reader.close();
+        reader.close(); //wrong area move
 
         System.out.println(file.getName() + " has been processed");
 
@@ -33,6 +33,7 @@ public class fileReader {
 
         }
         
+         
         return lines;
         
         
@@ -43,7 +44,6 @@ public class fileReader {
         System.out.println("Progam has started");
 
         String[] paths = {
-            "C:\\Users\\loljo\\Documents\\Programming Challenge\\Data-sets\\a_example.txt",
             "C:\\Users\\loljo\\Documents\\Programming Challenge\\Data-sets\\b_lovely_landscapes.txt",
             "C:\\Users\\loljo\\Documents\\Programming Challenge\\Data-sets\\c_memorable_moments.txt",
             "C:\\Users\\loljo\\Documents\\Programming Challenge\\Data-sets\\d_pet_pictures.txt",
@@ -59,6 +59,21 @@ public class fileReader {
         System.out.println("Finished " + path + " | Objects: " + processsedPhotos.size());
 
       }
+
+      List<Slide> slideshow = new ArrayList<>();
+      List<PhotoType> verticals = new ArrayList<>();
+
+        for (PhotoType p : processsedPhotos) {
+        if (!p.isVertical) {
+        slideshow.add(new Slide(p)); // Horizontal photos become slides instantly
+        } else {
+        verticals.add(p); // Save verticals to pair them up later
+        if (verticals.size() == 2) {
+            slideshow.add(new Slide(verticals.get(0), verticals.get(1)));
+            verticals.clear(); // Empty the temp list for the next pair
+        }
+    }
+}
 
     } 
 
@@ -87,17 +102,45 @@ public class fileReader {
 
 
 
-   static class PhotoType {
+ static class PhotoType {
     int id;
     boolean isVertical;
-    List<String> tags;
+    Set<String> tags; 
 
     public PhotoType(int id, String type, String[] tagsArray) {
         this.id = id;
         this.isVertical = type.trim().equalsIgnoreCase("V");
-        this.tags = new ArrayList<>(Arrays.asList(tagsArray));
+       
+        this.tags = new HashSet<>(Arrays.asList(tagsArray));
     }
-   } 
+}
+
+static class Slide {
+    List<Integer> ids = new ArrayList<>(); //Stores 1 or 2 Photo IDs
+    Set<String> tags = new HashSet<>(); //The combined "Toolbox" of tags
+
+    //Constructor for a single Horizontal photo
+
+    public Slide (PhotoType photo) {
+        this.ids.add(photo.id);
+        this.tags.addAll(photo.tags); //Copy all tags into the slide
+    }
+
+    //Constructor for two Vertical photos
+    public Slide (PhotoType p1, PhotoType p2) {
+        this.ids.add(p1.id);
+        this.ids.add(p2.id);
+    
+
+    //This is the merge!
+    //addAll takes everything from the photo's set and puts it in the slide's set
+
+    this.tags.addAll(p1.tags);
+    this.tags.addAll(p2.tags);
+
+    }
+
+    }
 
 }
 
